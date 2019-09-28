ce0448410139b79072ad6e09d117c14d51f59b2b
Jimmy <jimmy.maduro@hotmail.fr>
2019-09-28 13:14:13 +0200

    Initial commit

---
+++ app/src/main/java/com/sweethome/jimmy/go4lunch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sweethome.jimmy.go4lunch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
